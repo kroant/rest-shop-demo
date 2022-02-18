1f89ab8122da0d8c03819f6a15dad4abe18e4c38
kroant <kromera@rixo.cz>
2022-02-18 13:59:22 +0100

    sql code format fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -27,6 +27,6 @@ CREATE TABLE order_item (
 CREATE TABLE shedlock (
 	name       VARCHAR(64) NOT NULL PRIMARY KEY,
 	lock_until TIMESTAMP NOT NULL,
-    locked_at  TIMESTAMP NOT NULL,
-    locked_by  VARCHAR(255) NOT NULL
+	locked_at  TIMESTAMP NOT NULL,
+	locked_by  VARCHAR(255) NOT NULL
 );
